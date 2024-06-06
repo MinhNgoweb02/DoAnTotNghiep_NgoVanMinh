27f5898a7f68992fd0d076929bd2f59f5541b13a
MinhNgoweb02 <minhngo01022002@gmail.com>
2024-06-06 22:50:20 +0700

    Project DoAn

---
+++ Data/Services/IProductStockService.cs
@@ -0,0 +1,20 @@
+﻿using Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Services
+{
+    public interface IProductStockService
+    {
+        List<ProductSize> GetAll();
+        List<ProductSize> GetProductSizeByProductId(long productId);
+        ProductSize GetProductSizeByProductIdAndSizeId(long productId);
+        int GetStock(long productId);
+        void Insert(ProductSize productSize);
+        void Update(ProductSize productSize);
+        void InsertMany(List<ProductSize> productSizes);
+    }
+}
